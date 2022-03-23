ae84edc9b6d45d233b531dbbce278cc7e866cdc6
Adi Wabisabi <adi.wabisabi@redis.com>
2022-03-22 23:50:45 -0600

    make faceted search prettier

--- redisfi/web/research/__init__.py
+++ redisfi/web/research/__init__.py
@@ -18,20 +18,30 @@ def full_text():
     return render_template('research/results.html', **resp_data)
 
 @research.route('/vss')
-def vss():
-    query = request.args.get('query')
-    _filter = request.args.get('filter')
+def vss(query=None, _filter=None):
+    query = query or request.args.get('query')
+    _filter = _filter or request.args.get('filter')
     if _filter is not None:
         params = {'term':query, 'filter':_filter}
     else:
-        params = {'term':query}
-
-    print(params)
+        params = {'term':query, 'filter':'@FILING_YEAR:[2000 2022]'}
 
     url = current_app.config.get('VSS_URL')
     resp = requests.get(url, params=params)
     resp_data = resp.json()
+    resp_data['results'] = filter(lambda result: result['COMPANY_NAME'] != 'N/A', resp_data['results'])
     return render_template('research/results.html', **resp_data, facets=facets(query, serialize=False), query=query)
+
+@research.route('/faceted-search', methods=['POST'])
+def faceted_search():
+    data = request.form.to_dict(flat=False)
+    print(data)
+    _filter = ''
+    if 'companies' in data:
+        _filter += f'@COMPANY_NAME:({"|".join(data["companies"])})'
+
+    return vss(data['query'], _filter)
+
 
 
 
